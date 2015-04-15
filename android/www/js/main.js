4792d746f2ff3475fb49202cb533b960ad4b08f7
Nishanth Vijayan <nishanththegr8@gmail.com>
2015-04-15 08:14:33 +0530

    Fixed reload gif stuck bug.Plus refactoring

--- android/www/js/main.js
+++ android/www/js/main.js
@@ -36,12 +36,12 @@ function putdata(json)
 
   $.each(json.result.ongoing , function(i,post){ 
      
-     $("#ongoing").append('<li><br><h3  onclick="load(&quot;'+post.url+'&quot;)">'+post.Name+'</h3>\
-        <img class="contest_image" src="img/'+icon(post.Platform)+'"></img><br><br>\
-        <h4>End: '+post.EndTime+'</h4><br><br>\
-        <h4 class="share" onclick="socialShare(0,&quot;'+post.Name+'&quot;,&quot;'+post.url+'&quot;,&quot;'+post.EndTime+'&quot;);" >Tell your Friends</h4>\
-        </li><hr>');
-    });
+    $("#ongoing").append('<li><br><h3  onclick="load(&quot;'+post.url+'&quot;)">'+post.Name+'</h3>\
+      <img class="contest_image" src="img/'+icon(post.Platform)+'"></img><br><br>\
+      <h4>End: '+post.EndTime+'</h4><br><br>\
+      <h4 class="share" onclick="socialShare(0,&quot;'+post.Name+'&quot;,&quot;'+post.url+'&quot;,&quot;'+post.EndTime+'&quot;);" >Tell your Friends</h4>\
+      </li><hr>');
+  });
   
   $.each(json.result.upcoming , function(i,post){ 
     
@@ -60,10 +60,10 @@ function putdata(json)
         calendar_string = '<h4 onclick="addcalendarEvent(&quot;'+post.Name+'&quot;,&quot;'+post.url+'&quot;,&quot;'+post.StartTime+'&quot;,&quot;'+post.EndTime+'&quot;);" class="calendar">Add to Calendar</h4>';
       }
       $("#upcoming").append('<li><br><h3 onclick="load(&quot;'+post.url+'&quot;)">'+post.Name+'</h3>\
-      <img class="contest_image" src="img/'+icon(post.Platform)+'"></img><br><br>\
-      <h4>Start: '+post.StartTime+'</h4><br>\
-      <h4>Duration: '+post.Duration+'</h4><br>'+calendar_string+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
-      <h4 class="share" onclick="socialShare(1,&quot;'+post.Name+'&quot;,&quot;'+post.url+'&quot;,&quot;'+post.StartTime+'&quot;);" >Tell your Friends</h4></li><hr>');
+        <img class="contest_image" src="img/'+icon(post.Platform)+'"></img><br><br>\
+        <h4>Start: '+post.StartTime+'</h4><br>\
+        <h4>Duration: '+post.Duration+'</h4><br>'+calendar_string+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\
+        <h4 class="share" onclick="socialShare(1,&quot;'+post.Name+'&quot;,&quot;'+post.url+'&quot;,&quot;'+post.StartTime+'&quot;);" >Tell your Friends</h4></li><hr>');
     };
 
     var error   = function(message) {};
@@ -77,33 +77,41 @@ function putdata(json)
 
 function fetchdata(){
 
+  imgToggle();
+  req =  new XMLHttpRequest();
+  req.open("GET",'https://contesttrackerapi.herokuapp.com/',true);
+  req.send();
+  req.onload = function(){
+
+    res = JSON.parse(req.responseText);
+
+    data = JSON.stringify(res);
+    window.localStorage.setItem('last_collected_data', data);
+    
     imgToggle();
-    req =  new XMLHttpRequest();
-    req.open("GET",'https://contesttrackerapi.herokuapp.com/',true);
-    req.send();
-    req.onload = function(){
+    putdata(res);
+  };
+  req.onerror = function(){
+    imgToggle();
+    navigator.notification.alert("Connection Failed",function() {},"Error","OK");
+  };
+}
 
-      res = JSON.parse(req.responseText);
-
-      data = JSON.stringify(res);
-      window.localStorage.setItem('last_collected_data', data);
-      imgToggle();
-      putdata(res);
-    };
-    req.onerror = function(){
-      imgToggle();
-      navigator.notification.alert("Connection Failed",function() {},"Error","OK");
-    };
+function restoredata(){
+  if(window.localStorage.getItem('last_collected_data')){
+    var localData = JSON.parse(window.localStorage.getItem('last_collected_data'));
+    putdata(localData);
+  }
 }
 
 function load(url){
   navigator.notification.confirm(
     "Would you like to open the contest page?",
     function( index ) {
-        switch ( index ) {
-            case 2:
-                window.open(url, "_system");
-                break;
+      switch ( index ) {
+        case 2:
+          window.open(url, "_system");
+          break;
         }
     },
     "Confirm", // a title
@@ -122,8 +130,7 @@ function addcalendarEvent(name,url,StartTime,EndTime){
   var notes = " ";
   var success = function(message) { 
     navigator.notification.alert("'"+name+"'  added to Calendar",function() {},"Notification","OK");
-    var localData = JSON.parse(window.localStorage.getItem('last_collected_data'));
-    putdata(localData);
+    restoredata();   
   };
   var error = function(message) { };
 
@@ -133,6 +140,7 @@ function addcalendarEvent(name,url,StartTime,EndTime){
   window.plugins.calendar.createEvent(title,eventLocation,notes,s,e,success,error);
   
 }
+
 function delcalendarEvent(name,url,StartTime,EndTime){
 
   startTime = Date.parse(StartTime)
@@ -144,32 +152,33 @@ function delcalendarEvent(name,url,StartTime,EndTime){
   var notes = " ";
   var success = function(message) { 
     navigator.notification.alert("'"+name+"'  deleted from Calendar",function() {},"Notification","OK");
-    var localData = JSON.parse(window.localStorage.getItem('last_collected_data'));
-    putdata(localData);
+    restoredata();
   };
   var error = function(message) { };
 
   window.plugins.calendar.deleteEvent(title,eventLocation,notes,s,e,success,error)
   
 }
+
 function socialShare(status,name,url,Time){
   navigator.notification.confirm(
     "Are you sure you want to tell others about this contest? ",
     function( index ) {
-        switch ( index ) {
-            case 2:
-                if(status==1){
-                  window.plugins.socialsharing.share( 'Hey, Check out this coding contest: \n'+name+' \nLink: '+ url + " \nStarts at: "+Time );
-                }else{
-                  window.plugins.socialsharing.share( 'Hey, Check out this coding contest: \n'+name+' , taking place now at '+ url + " \nEnds at: "+Time );
-                }
-                break;
-        }
+      switch ( index ) {
+        case 2:
+          if(status==1){
+            window.plugins.socialsharing.share( 'Hey, Check out this coding contest: \n'+name+' \nLink: '+ url + " \nStarts at: "+Time,'Coding Contest' );
+          }else{
+            window.plugins.socialsharing.share( 'Hey, Check out this coding contest: \n'+name+' , taking place now at '+ url + " \nEnds at: "+Time,'Coding Contest' );
+          }
+          break;
+      }
     },
     "Confirm", // a title
     [ "No","Yes" ]    // text of the buttons
   );
 }
+
 function imgToggle(){
   src = $('.loading').attr('src');
   if(src=="img/refresh-white.png") $(".loading").attr("src","img/ajax-loader.gif");
@@ -177,10 +186,9 @@ function imgToggle(){
 }
 
 document.addEventListener("deviceready", function(){
-  if(window.localStorage.getItem('last_collected_data')){
-      var localData = JSON.parse(window.localStorage.getItem('last_collected_data'));
-      putdata(localData);
-  }
+
+  restoredata();
+
   fetchdata();
   setInterval(function(){
     fetchdata();
@@ -191,10 +199,15 @@ document.addEventListener("deviceready", function(){
   $(".loading").click(function(){
     src = $('.loading').attr('src');
     if(src=="img/refresh-white.png") fetchdata();
+    
+    // the loading gif get stucks sometimes and starts spinning again only when the 
+    // user scrolls or selects some item.This works around this by scrolling down & up by 1px.
+    window.scrollBy(0,1);
+    window.scrollBy(0,-1);
   });
 
   $(".info").click(function(){
-    navigator.notification.alert("Tap on the contest to visit the contest page.\n\nTap on Add to Calendar/Delete from Calendar to add/delete the contest to your calendar.\n\nTap on 'Tell your Friends' to let others know about the contest.\n\nHappy Coding!",function() {},"Instructions","OK");
+    navigator.notification.alert("Tap on the contest name to visit the contest page.\n\nTap on Add to Calendar/Delete from Calendar to add/delete the contest to/from your calendar.\n\nTap on 'Tell your Friends' to let others know about the contest.\n\nHappy Coding!",function() {},"Instructions","OK");
   });
 
 });

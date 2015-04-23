8e314f221452a053eba8e97f4874123300752a3a
Nishanth Vijayan <nishanththegr8@gmail.com>
2015-04-24 01:19:15 +0530

    Removed Source Code and added star button.Changed V.no.

--- firefox/data/popup.js
+++ firefox/data/popup.js
@@ -43,7 +43,7 @@ function putdata(json)
     
       $("#ongoing").append('<a  data='+'"'+post.url+'"'+'>\
       	<li><br><h4>'+post.Name+'</h4>\
-      	<img title="'+post.Platform+'" src="'+icon(post.Platform)+'"></img><br>\
+      	<img alt="'+post.Platform+'" title="'+post.Platform+'" src="'+icon(post.Platform)+'"></img><br>\
       	<h5>End: '+timezonePerfectEndTime+'</h5><br>\
       	</li><hr></a>');
     }
@@ -70,14 +70,14 @@ function putdata(json)
     if(sT<curTime && eT>curTime){
       $("#ongoing").append('<a  data='+'"'+post.url+'"'+'>\
         <li><br><h4>'+post.Name+'</h4>\
-        <img title="'+post.Platform+'" src="'+icon(post.Platform)+'"></img><br>\
+        <img alt="'+post.Platform+'" title="'+post.Platform+'" src="'+icon(post.Platform)+'"></img><br>\
         <h5>End: '+timezonePerfectEndTime+'</h5><br>\
         </li><hr></a>');
     }
     else if(sT>curTime && eT>curTime){
       $("#upcoming").append('<a  data='+'"'+post.url+'"'+'>\
         <li><br><h4>'+post.Name+'</h4>\
-        <img title="'+post.Platform+'" src="'+icon(post.Platform)+'"></img><br>\
+        <img alt="'+post.Platform+'" title="'+post.Platform+'" src="'+icon(post.Platform)+'"></img><br>\
         <h5>Start: '+timezonePerfectStartTime+'</h5><br>\
         <h5>Duration: '+post.Duration+'</h5><br>\
         <h5 data='+calendarLink+' class="calendar">Add to Calendar</h5>\
@@ -158,5 +158,9 @@ $(document).ready(function(){
     src = $('.loading').attr('src');
     if(src=="img/refresh-white.png") fetchdata();
   });
+
+  setTimeout(function(){
+    $("footer a:first-child").after('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span><iframe src="https://ghbtns.com/github-btn.html?user=nishanthvijayan&repo=codercalendar&type=star&count=true" frameborder="0" scrolling="0" width="100px" height="20px"></iframe></span>');
+  },1000);
 });
 

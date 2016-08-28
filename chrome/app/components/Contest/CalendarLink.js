6973d0cfb5c1e1cfdf9b916acf2c7d64ea13f9f0
Nishanth Vijayan <nishanththegr8@gmail.com>
2016-08-29 00:37:55 +0530

    Require UtilHelpers in CalendarLink component for Calendarlink creation

--- chrome/app/components/Contest/CalendarLink.js
+++ chrome/app/components/Contest/CalendarLink.js
@@ -1,12 +1,13 @@
 var React = require('react');
+var UtilHelpers = require('../../util');
 
 var CalendarLink = React.createClass({
     constructGoogleCalendarLink: function(contest){
         var curTime  = new Date();
         var startTime = Date.parse(contest.StartTime);
         var endTime = Date.parse(contest.EndTime);
-        var s = new Date(changeTimezone(startTime).getTime() - ((curTime).getTimezoneOffset()*60000 )).toISOString().slice(0,19).replace(/-/g,"").replace(/:/g,"");
-        var e = new Date(changeTimezone(endTime).getTime() - ((curTime).getTimezoneOffset()*60000 )).toISOString().slice(0,19).replace(/-/g,"").replace(/:/g,"");    
+        var s = new Date(UtilHelpers.changeTimezone(startTime).getTime() - ((curTime).getTimezoneOffset()*60000 )).toISOString().slice(0,19).replace(/-/g,"").replace(/:/g,"");
+        var e = new Date(UtilHelpers.changeTimezone(endTime).getTime() - ((curTime).getTimezoneOffset()*60000 )).toISOString().slice(0,19).replace(/-/g,"").replace(/:/g,"");    
         var calendarTime = s+'/'+e
         return "https://www.google.com/calendar/render?action=TEMPLATE&text="+encodeURIComponent(contest.Name)+
                    "&dates="+calendarTime+"&location="+contest.url+"&pli=1&uid=&sf=true&output=xml#eventpage_6";
